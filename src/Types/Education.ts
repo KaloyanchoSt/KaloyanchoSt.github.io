55e7acf5a8db5d4a723750dba558697e0b4116b3
Kaloyan <kaloyanchost@gmail.com>
2023-09-24 17:29:41 +0300

    added Types

---
+++ src/Types/Education.ts
@@ -0,0 +1,8 @@
+export interface EducationViewModel {
+  ID: number;
+  From: Date;
+  Until: Date;
+  Institution: string;
+  Specialty: string;
+  Subjects: string[];
+}
---
+++ src/Types/Project.ts
@@ -0,0 +1,8 @@
+export interface ProjectViewModel {
+    ID: number;
+    From: Date;
+    Until: Date;
+    Name: string;
+    Specialty: string;
+    Technologies: string[];
+}
