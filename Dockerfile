75a752fe2e620c74e52f734bdf01fc89c9faad5d
Kaloyan <kaloyanchost@gmail.com>
2023-09-29 21:25:15 +0300

    cleanup

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:lts-alpine as builder
-
-WORKDIR /app
-COPY . ./
-RUN npm install && npm run build
-
-FROM nginx:alpine
-
-COPY --from=builder /app/dist /usr/share/nginx/html
--- src/Types/Project.ts
+++
@@ -1,9 +0,0 @@
-export interface ProjectViewModel {
-    ID: number;
-    From: Date;
-    Until: Date;
-    Name: string;
-    Description: string;
-    Technologies: string[];
-  }
-  
